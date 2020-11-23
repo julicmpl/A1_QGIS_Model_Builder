d16800a8fb3c7b161680c250c559f38686e0617a
julicmpl <juliacamp@web.de>
2020-11-23 15:39:20 +0100

    Aufgabe_2

---
+++ calculate_slope_hillshade.bat
@@ -0,0 +1,5 @@
+
+C:\Users\Julia\Documents\MA\FOSS\Aufgabe 2>ogr2ogr -clipsrcwhere "NAME_2="Izola"" -f "ESRI Shapefile" Izola.shp gadm36_SVN.gpkg gadm36_SVN_2
+C:\Users\Julia\Documents\MA\FOSS\Aufgabe 2>gdalwarp -dstnodata -9999 -t_srs EPSG:32632 -cutline Izola.shp -crop_to_cutline dem_merge.tif dem_merge_clip_Izola.tif
+C:\Users\Julia\Documents\MA\FOSS\Aufgabe 2>gdaldem slope dem_merge_clip_Izola.tif slope.tif -p
+C:\Users\Julia\Documents\MA\FOSS\Aufgabe 2>gdaldem hillshade dem_merge_clip_Izola.tif hillshade.tif
