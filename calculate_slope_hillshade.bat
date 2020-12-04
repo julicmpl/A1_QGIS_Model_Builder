fff43630eab2332724f8136b93f86ca6311115c4
julicmpl <74054454+julicmpl@users.noreply.github.com>
2020-12-04 19:25:29 +0100

    Update calculate_slope_hillshade.bat

--- calculate_slope_hillshade.bat
+++ calculate_slope_hillshade.bat
@@ -1,4 +1,5 @@
-
+C:\Users\Julia\Documents\MA\FOSS\Aufgabe 2>gdal_merge -o dem_merge.tif N45E014.hgt n45_e013_1arc_v3.tif
+C:\Users\Julia\Documents\MA\FOSS\Aufgabe 2>gdalbuildvrt -separate dem_buildvrt.vrt N45E014.hgt n45_e013_1arc_v3.tif
 C:\Users\Julia\Documents\MA\FOSS\Aufgabe 2>ogr2ogr -clipsrcwhere "NAME_2="Izola"" -f "ESRI Shapefile" Izola.shp gadm36_SVN.gpkg gadm36_SVN_2
 C:\Users\Julia\Documents\MA\FOSS\Aufgabe 2>gdalwarp -dstnodata -9999 -t_srs EPSG:32632 -cutline Izola.shp -crop_to_cutline dem_merge.tif dem_merge_clip_Izola.tif
 C:\Users\Julia\Documents\MA\FOSS\Aufgabe 2>gdaldem slope dem_merge_clip_Izola.tif slope.tif -p
